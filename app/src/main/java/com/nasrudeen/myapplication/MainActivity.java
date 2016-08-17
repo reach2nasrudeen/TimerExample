cc7317491e3fee5b2bd71108064bdfb2501c0764
reach2nasrudeen <reach2nasrudeen@gmail.nasrudeen@gmail.com>
2016-08-17 16:22:34 +0530

    ViewFlipper Animation Added

--- app/src/main/java/com/nasrudeen/myapplication/MainActivity.java
+++ app/src/main/java/com/nasrudeen/myapplication/MainActivity.java
@@ -4,23 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     TextView hTextView;
+    ViewFlipper viewFlipper;
     private String[] values;
     private int position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipperSpecialNews);
         values = new String[]{"Test 1","Test 2","Test 3","Test 4","Test 5"};
-        hTextView = (TextView)findViewById(R.id.idTextView);
+        hTextView = (TextView)findViewById(R.id.txtspecialview);
         MyTimerTask myTask = new MyTimerTask();
         Timer myTimer = new Timer();
-        myTimer.schedule(myTask, 3000, 1500);
+        myTimer.schedule(myTask, 3000, 5000);
     }
     class MyTimerTask extends TimerTask {
         public void run() {
@@ -30,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
                     if(position >= values.length){
                         position = 0;
                     }
-                    hTextView.append("\n"+values[position]);
+                    AnimationFactory.flipTransition(viewFlipper, AnimationFactory.FlipDirection.LEFT_RIGHT);
+                    hTextView.setText("\n"+values[position]);
                     position = position+1;
                 }
             });
